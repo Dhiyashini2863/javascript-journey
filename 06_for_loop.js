96d996fe41596ae4f2f33d38e24521cdb7afdbd3
Dhiyashini2863 <iamdhiyashini@gmail.com>
2023-09-02 15:57:08 +0530

    For loop

---
+++ 06_for_loop.js
@@ -0,0 +1,15 @@
+//Numbers between 1 to 10
+console.log("Numbers between 1 to 10")
+for(let i=1;i<=10;i++)
+    console.log(i)
+
+//Numbers from 1 to 10 incremented by 2
+console.log("Numbers from 1 to 10 incremented by 2")
+for(let j=1;j<=10;j=j+2)
+    console.log(j)
+
+//Numbers from 1 to 10 in reverse order
+console.log("Numbers between 1 to 10 in reverse order")
+for(let k=10;k>=1;k--)
+console.log(k)
+   
---
+++ 06_tables.js
@@ -0,0 +1,8 @@
+//Multiplication tables
+let num=parseInt(prompt("Enter a number:"))
+for(let i=1;i<=10;i++)
+{
+    let answer=i*num
+    console.log(`${i}x${num}=${answer}`)
+}
+    

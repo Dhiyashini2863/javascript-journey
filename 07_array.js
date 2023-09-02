fa8a3de306a301156df19f10d746b6141549b49f
Dhiyashini2863 <iamdhiyashini@gmail.com>
2023-09-02 16:32:05 +0530

    Array Concepts

---
+++ 07_array.js
@@ -0,0 +1,9 @@
+var flowers=["lily","rose","tulip","jasmine"]
+//display specific element
+console.log(flowers[1])
+//change an element
+flowers[3]="sunflower"
+//display all elements
+console.log(flowers)
+//length of array
+console.log(flowers.length)
---
+++ 07_task.js
@@ -0,0 +1,3 @@
+var flowers=["lily","rose","tulip","jasmine"]
+for(let i=0;i<=(flowers.length)-1;i++)
+    console.log(flowers[i])

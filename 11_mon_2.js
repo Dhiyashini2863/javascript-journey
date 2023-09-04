ae6bef9f279ed683a1c846f4abb0809d499835bc
Dhiyashini2863 <iamdhiyashini@gmail.com>
2023-09-04 16:42:47 +0530

    1st Monday work

---
+++ 11_mon_2.js
@@ -0,0 +1,11 @@
+// Initialize variables
+let sum = 0;
+let count = 1;
+
+// Use a for loop to iterate and calculate the sum
+for (count = 1; count <= 10; count = count + 1) {
+    sum = sum + count;
+}
+
+// Display the sum
+console.log("The sum of the first 10 numbers is: " + sum);
